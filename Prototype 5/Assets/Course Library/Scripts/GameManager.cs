b4ffd0312a03105e07c70e8afdd826e43ae55ddd
BEANZEXTREME <59098911+BEANZEXTREME@users.noreply.github.com>
2020-05-08 18:59:13 -0700

    Challenge 5
    done

--- Prototype 5/Assets/Course Library/Scripts/GameManager.cs
+++ Prototype 5/Assets/Course Library/Scripts/GameManager.cs
@@ -57,7 +57,7 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    public void StartGame()
+    public void StartGame(int difficulty)
     {
         isGameActive = true;
         score = 0;
@@ -65,5 +65,6 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
         titleScreen.gameObject.SetActive(false);
+        spawnRate /= difficulty;
     }
 }
